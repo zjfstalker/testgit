8f56131349a62fc146ce96c54d9b9511a0cbae61
zjfstalker <zjfstalker@gmail.com>
2018-11-22 09:12:58 +0800

    web_hooks config

--- hook.php
+++ hook.php
@@ -7,6 +7,14 @@
 
    */
 $string = "<?php \n return " . var_export($_POST, true) . "\n?>";
-file_put_contents('var.txt', $string);  //输出一个github上的回调传回的参数，至于你要用来做什么，可自行研究。
-exec('./web_hooks.sh', $res);  //使用php来执行bash命令
-var_dump($res);  //如果自动更新不成功，可手动执行这个文件，查看错误。
+
+file_put_contents('var.txt', $string); 
+//输出一个github上的回调传回的参数，至于你要用来做什么，可自行研究。
+
+$res = '';
+
+exec('./web_hooks.sh', $res);
+//使用php来执行bash命令
+
+var_dump($res);
+//如果自动更新不成功，可手动执行这个文件，查看错误。
