481ec6985a57aa65a9d7bda3a3918edcb76e9d9f
zjfstalker <root@bogon.(none)>
2018-11-22 09:03:16 +0800

    web_hooks config

--- hook.php
+++ hook.php
@@ -1,3 +1,12 @@
 <?php
 
-echo exec('git pull');
+ /**
+
+  - 给github提供的同步代码到测试服务器的hook
+  - exec函数一般在php.ini文件中是关闭的，请打开
+
+   */
+$string = "<?php \n return " . var_export($_POST, true) . "\n?>";
+file_put_contents('var.txt', $string);  //输出一个github上的回调传回的参数，至于你要用来做什么，可自行研究。
+exec('./web_hooks.sh', $res);  //使用php来执行bash命令
+var_dump($res);  //如果自动更新不成功，可手动执行这个文件，查看错误。
---
+++ web_hooks.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git commit -a -m '下拉备份'
+
+git pull
