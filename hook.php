813f9230ab7b5714144c60234632cfad9a1a97bc
zjfstalker <zjfstalker@gmail.com>
2018-11-22 10:08:58 +0800

    下拉备份

--- hook.php
+++ hook.php
@@ -1,16 +1,10 @@
 <?php
 
- /**
-
-  - 给github提供的同步代码到测试服务器的hook
+  /*
+  - github提供的同步代码到测试服务器的hook
   - exec函数一般在php.ini文件中是关闭的，请打开
-
    */
 $string = "<?php \n return " . var_export($_POST, true) . "\n?>";
-
-
-file_put_contents('var.txt', $string); 
-
+file_put_contents('var.txt', $string); 
 exec('./web_hooks.sh', $res);
-
 var_dump($res);
