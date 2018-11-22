6cdc1e86accb91e8960748a6f2ba83dc97c34391
zjfstalker <zjfstalker@gmail.com>
2018-11-22 09:42:57 +0800

    auto commit

--- hook.php
+++ hook.php
@@ -8,8 +8,13 @@
    */
 $string = "<?php \n return " . var_export($_POST, true) . "\n?>";
 
-file_put_contents('var.txt', $string); 
-//输出一个github上的回调传回的参数，至于你要用来做什么，可自行研究。
+if (!empty($string)) {
+
+    file_put_contents('var.txt', $string); 
+    //输出一个github上的回调传回的参数，至于你要用来做什么，可自行研究。
+
+
+}
 
 exec('./web_hooks.sh', $res);
 //使用php来执行bash命令
